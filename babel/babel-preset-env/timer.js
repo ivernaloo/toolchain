3892e0b2ecb6b66c1c926d4a67d531bb5c68fd96
aloo <aloocs@gmail.com>
2017-02-15 18:38:17 +0800

    一地鸡毛，明天来理清

---
+++ babel/babel-preset-env/timer.js
@@ -0,0 +1,15 @@
+
+function sleep(ms = 0){
+    return new Promise(function(resolve, reject){
+        setTimeout(resolve, ms)
+    });
+}
+
+async function test(){
+    for (let i = 0; i < 10; i++){
+        await sleep(500);
+        console.log(`i=${i}`);
+    }
+}
+
+test().then(function(){console.log('done')});
