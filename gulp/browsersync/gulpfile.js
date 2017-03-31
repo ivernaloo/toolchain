8d95a5451f0b9399b955319e010f7e23e2f7dd92
aloo <aloocs@gmail.com>
2017-03-31 11:27:54 +0800

    update the auto reload

--- gulp/browsersync/gulpfile.js
+++ gulp/browsersync/gulpfile.js
@@ -13,3 +13,20 @@ gulp.task('browser-sync', function(){
 
     gulp.watch("*").on("change", reload);
 });
+
+
+// static server
+gulp.task('browser-sync2', function(){
+    browserSync.init({
+        server: {
+            baseDir: "src/",
+            routes: {
+                "/node_modules": "node_modules"
+            }
+        },
+        port: 3033,
+        files: ["src/res/js/*.js","src/*.html"]
+    });
+
+    //gulp.watch("**/*").on("change", reload);
+});
